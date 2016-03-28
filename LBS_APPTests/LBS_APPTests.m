8b07a13862d9aa45707a0bd99918357a26421df8
msi <msi@msidemac.local>
2016-03-28 05:44:19 -0700

    Initial Commit

---
+++ LBS_APPTests/LBS_APPTests.m
@@ -0,0 +1,39 @@
+//
+//  LBS_APPTests.m
+//  LBS_APPTests
+//
+//  Created by msi on 16/3/28.
+//  Copyright © 2016年 msi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LBS_APPTests : XCTestCase
+
+@end
+
+@implementation LBS_APPTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
